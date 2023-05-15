ec77469ad7bb868e1a35a8e32a9eb2d5d399ada2
Priydarshini Karanth <kpriya1203@gmail.com>
2023-05-15 15:49:41 +0530

    search form ui implementation

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,18 @@
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input type="text" placeholder="cat" className="search-input" />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
