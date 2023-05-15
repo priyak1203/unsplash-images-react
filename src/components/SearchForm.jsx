e166715f61c8aed1705fde8ebd05a56caf5166a8
Priydarshini Karanth <kpriya1203@gmail.com>
2023-05-15 21:13:16 +0530

    searching images implementation

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,12 +1,25 @@
+import { useGlobalContext } from '../context';
+
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    setSearchTerm(searchValue);
   };
+
   return (
     <section>
       <h1 className="title">unsplash images</h1>
       <form className="search-form" onSubmit={handleSubmit}>
-        <input type="text" placeholder="cat" className="search-input" />
+        <input
+          type="text"
+          placeholder="cat"
+          className="search-input"
+          name="search"
+        />
         <button type="submit" className="btn">
           search
         </button>
--- src/context.jsx
+++ src/context.jsx
@@ -4,6 +4,7 @@ const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('cat');
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
@@ -11,7 +12,9 @@ const AppProvider = ({ children }) => {
     document.body.classList.toggle('dark-theme', newDarkTheme);
   };
   return (
-    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
       {children}
     </AppContext.Provider>
   );
