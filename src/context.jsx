51031d97f81993decf0af04b08365b06fa13e694
Priydarshini Karanth <kpriya1203@gmail.com>
2023-05-16 12:33:55 +0530

    dark mode preference using javascript

--- src/context.jsx
+++ src/context.jsx
@@ -1,9 +1,17 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme:dark)'
+  ).matches;
+  console.log(prefersDarkMode);
+  return prefersDarkMode;
+};
+
 const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState('cat');
 
   const toggleDarkTheme = () => {
@@ -11,6 +19,11 @@ const AppProvider = ({ children }) => {
     setIsDarkTheme(newDarkTheme);
     document.body.classList.toggle('dark-theme', newDarkTheme);
   };
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkTheme);
+  }, []);
+
   return (
     <AppContext.Provider
       value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
